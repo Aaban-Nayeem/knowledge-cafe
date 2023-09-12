f6c2224e21e3c41f22bb18c321d8c007893b082b
Aaban Nayeem <aabannayeem4358@gmail.com>
2023-09-12 17:35:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="m-5 bg-amber-300 p-10 text-center text-3xl font-bold text-emerald-500 hover:bg-emerald-300 hover:text-black ">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
